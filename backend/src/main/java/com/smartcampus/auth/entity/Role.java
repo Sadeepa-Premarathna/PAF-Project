3fbe1fede3ffca83ca1f0cc01b91941d93eae8ac
Sadeepa Premarathna <sadeepapremarathna1623@gmail.com>
2026-04-19 23:38:12 +0530

    Delete backend directory

--- backend/src/main/java/com/smartcampus/auth/entity/Role.java
+++
@@ -1,8 +0,0 @@
-package com.smartcampus.auth.entity;
-
-public enum Role {
-    USER,
-    ADMIN,
-    TECHNICIAN,
-    MANAGER
-}
--- backend/src/main/java/com/smartcampus/auth/exception/AccountDisabledException.java
+++
@@ -1,6 +0,0 @@
-package com.smartcampus.auth.exception;
-
-public class AccountDisabledException extends RuntimeException {
-    public AccountDisabledException(String message) { super(message); }
-    public AccountDisabledException(String message, Throwable cause) { super(message, cause); }
-}
